113719e16fe9625262c9a700fa6b32d26ba47bdb
kamil <masacz88@gmail.com>
2021-12-05 00:29:28 +0100

    Cw 1. Kammas

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,18 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
+        {
+            System.out.println("Dzień dobry!");
+            System.out.println("Chciałbym się przedstawić");
+            System.out.println("Na" + " " + "imię mam " + "Kamil!");
 
+            System.out.printf("To %s mój %d raz z Javą.\n", "jest", 1);
+
+            System.out.print("Tutaj");
+            System.out.print(" będzie krótki");
+            System.out.print(" zwięzły tekst.\n");
+
+
+        }
     }
 }
