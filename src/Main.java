d5fa95d87acbde1705a82221190bfdacb1e1025d
kamil <masacz88@gmail.com>
2021-12-05 01:26:28 +0100

    Cw.5 Kammas

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,53 @@ public class Main {
         /* zad.1 podziel swój wiek przez modulo 3 i jeśli twój wiek zwraca 0 wyświetl napis "Podzielny przez 3",
          * w innym wypadku "Niepodzielny przez 3" użyj kontrukcji if else
          * */
+        {
+            Scanner scan = new Scanner(System.in);
 
+            System.out.println("Podaj swój wiek");
+            int podanyWiek = scan.nextInt();
+
+            if ((podanyWiek) % 2 == 0)
+            {
+                System.out.println("Wiek podzielny przez 3");
+            }
+            else
+            {
+                System.out.println("Wiek nie podzielny przez 3");
+            }
+            System.out.println();
+        }
 
         /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
 
+       {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Podaj nr indeksu");
+            int numerIndex = scan.nextInt();
+
+            String czyParzysta = ((numerIndex) % 2 == 0) ? "parzysta" : "nieparzysta";
+            System.out.println(czyParzysta);
+            System.out.println();
+       }
 
         /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
          * i stwórz konstrukcję else if z dowolnymi komunikatami */
 
+        {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Podaj dowolną liczbe zmiennoprzecinkową w zakresie od 0,1 do 0,99 i sprawdz czy wygrałeś");
+            Double przecinekLiczba = scan.nextDouble();
 
+            if ((przecinekLiczba) == 0.88)
+            {
+                System.out.println("Wygrałeś");
+            }
+            else
+            {
+                System.out.println("Przykro mi spróbuj ponownie");
+            }
+        }
     }
 }
+
+
