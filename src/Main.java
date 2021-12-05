9eac02c6944ee6d54416053d1cca8153d5e176b6
kamil <masacz88@gmail.com>
2021-12-05 01:39:26 +0100

    Cw.6 Kammas

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,18 @@ public class Main {
 
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
+        {
+            Scanner scan = new Scanner(System.in);
 
+            double[] tab = new double[5];
+            for (int i=0; i<tab.length; i++)
+            {
+                System.out.println("Podaj liczbe zmiennoprzecinkową");
+                double tmp = scan.nextDouble();
+                tab[i] = tmp;
+            }
 
+            System.out.println("tab[5] =  " + tab[0] + ", " + tab[1] + ", " + tab[2] + ", " + tab[3] + ", " + tab[4]);
+        }
     }
 }
